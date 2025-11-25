f0fe62784061f0c884722ac7a20d80aad60f9876
p4t000 <leivagomezpato@gmail.com>
2025-11-25 16:32:01 -0300

    Update UsuarioDAO.py

--- dao/UsuarioDAO.py
+++ dao/UsuarioDAO.py
@@ -1,11 +1,14 @@
+from models.Usuario import Usuario
+from models.Conectar import Conectar
+
 class UsuarioDAO:
     def __init__(self, usuario:Usuario):
-        self.__conexion = conectar()
+        self.__conexion = Conectar()
         self.__usuario = usuario
         
     def iniciar_sesion(self):
         sql = '''
-        SELECT e.nombre, e.apellido 
+        SELECT e.nombre, e.apellido, e.direccion, e.telefono, e.email, e.empleado_id, e.usuario_id, e.departamento_id, e.rol_id 
         FROM usuario u JOIN empleado e 
         ON u.usuario_id = e.usuario_id
         WHERE nombre_usuario = %s AND hash_password = %s;'''
@@ -13,5 +16,12 @@ class UsuarioDAO:
         if datos:
             self.__usuario.nombre = datos['nombre']
             self.__usuario.apellido = datos['apellido']
+            self.__usuario.direccion = datos['direccion']
+            self.__usuario.telefono = datos['telefono']
+            self.__usuario.email = datos['email']
+            self.__usuario.empleado_id = datos['empleado_id']
+            self.__usuario.usuario_id = datos['usuario_id']
+            self.__usuario.departamento_id = datos['departamento_id']
+            self.__usuario.rol_id = datos['rol_id']
             return True
         return False
