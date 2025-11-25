6c011e8f82d2ff653702e5266c408fe3a56bd3ae
p4t000 <leivagomezpato@gmail.com>
2025-11-25 16:35:17 -0300

    Update Usuario.py

--- models/Usuario.py
+++ models/Usuario.py
@@ -1,8 +1,18 @@
 from models.Empleado import Empleado
 
 class Usuario(Empleado):
-    def __init__(self, nombre = None, apellido = None, usuario:str = None, password:str = None):
-        super().__init__(nombre, apellido)
+    def __init__(self, nombre = None, 
+                 apellido = None, 
+                 direccion = None, 
+                 telefono = None, 
+                 email = None, 
+                 empleado_id = None, 
+                 usuario_id = None, 
+                 departamento_id = None, 
+                 rol_id = None,
+                 usuario:str = None,
+                 password:str = None):
+        super().__init__(nombre, apellido, direccion, telefono, email, empleado_id, usuario_id, departamento_id, rol_id)
         self.__usuario = usuario
         self.__password = password
         
